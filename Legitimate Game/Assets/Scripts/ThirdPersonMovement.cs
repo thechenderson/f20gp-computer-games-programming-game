5aa3a82c8af2597db05c4d9fdf26083e98a45fff
thechenderson <chris.henderson1999@gmail.com>
2021-02-03 12:25:44 +0000

    Basic scene creation

---
+++ Legitimate Game/Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public CharacterController controller;     
+
+    public float moveSpeed = 6;
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
