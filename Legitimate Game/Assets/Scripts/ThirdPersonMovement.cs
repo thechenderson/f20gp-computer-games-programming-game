267f5d41ae5276a5801305a3ec41c829cdc46279
thechenderson <chris.henderson1999@gmail.com>
2021-02-05 13:29:44 +0000

    Unity Files + some external assets

--- Legitimate Game/Assets/Scripts/ThirdPersonMovement.cs
+++ Legitimate Game/Assets/Scripts/ThirdPersonMovement.cs
@@ -5,11 +5,30 @@ using UnityEngine;
 public class ThirdPersonMovement : MonoBehaviour
 {
     public CharacterController controller;     
+    public Transform cam;
+    public float moveSpeed = 10f;
 
-    public float moveSpeed = 6;
+    public float smoothTurnTime = 0.1f;
+    float turnSmoothVelocity;
+
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+    
+        if(direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothVelocity, smoothTurnTime);
+
+
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDirection.normalized * moveSpeed * Time.deltaTime);
+        }
+    
     }
 }
