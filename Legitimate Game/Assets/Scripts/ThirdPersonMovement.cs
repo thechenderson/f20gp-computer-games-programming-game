0cbf87197479d0e39d9c4a6e9f2bf486d6588bca
thechenderson <chris.henderson1999@gmail.com>
2021-02-15 17:17:54 +0000

    Control character and collide with objects

--- Legitimate Game/Assets/Scripts/ThirdPersonMovement.cs
+++ Legitimate Game/Assets/Scripts/ThirdPersonMovement.cs
@@ -21,7 +21,7 @@ public class ThirdPersonMovement : MonoBehaviour
         if(direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothVelocity, smoothTurnTime);
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, smoothTurnTime);
 
 
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
