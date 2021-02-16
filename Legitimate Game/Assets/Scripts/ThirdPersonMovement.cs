81cce5286f554d92f940eba6aefa7e97de101ad5
Christopher Henderson <chris.henderson1999@gmail.com>
2021-02-16 16:22:04 +0000

    Fixed gravity and added broken jump

--- Legitimate Game/Assets/Scripts/ThirdPersonMovement.cs
+++ Legitimate Game/Assets/Scripts/ThirdPersonMovement.cs
@@ -6,18 +6,37 @@ public class ThirdPersonMovement : MonoBehaviour
 {
     public CharacterController controller;     
     public Transform cam;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
     public float moveSpeed = 10f;
-
-    public float smoothTurnTime = 0.1f;
+    public float jumpHeight = 5f;
+    float smoothTurnTime = 0.1f;
     float turnSmoothVelocity;
+    public float gravity = -9.81f;
+    bool isGrounded;
+
+
+    Vector3 velocity;
 
     // Update is called once per frame
     void Update()
     {
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-    
+
+
         if(direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
@@ -29,6 +48,14 @@ public class ThirdPersonMovement : MonoBehaviour
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDirection.normalized * moveSpeed * Time.deltaTime);
         }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -0.5f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity*Time.deltaTime);
     
     }
 }
