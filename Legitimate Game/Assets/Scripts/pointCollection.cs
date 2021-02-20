37043de47e4259ff5a1dd480cb9fca2d768d53e7
Christopher Henderson <chris.henderson1999@gmail.com>
2021-02-20 17:34:08 +0000

    Removed uniused assets. Added more level design stuff

---
+++ Legitimate Game/Assets/Scripts/pointCollection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pointCollection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.name == "Player") {
+          pointsUI.points += 1;
+          Debug.Log ("Collected Point Sphere");
+          Destroy(gameObject);
+        }
+        
+
+    }
+
+
+}
---
+++ Legitimate Game/Assets/Scripts/pointsUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pointsUI : MonoBehaviour
+{
+    public static int points = 0;
+    public Text pointText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        pointText.text = "Points: " + points;    
+    }
+}
