c364bb1cb02e06dee92c074e79eca80fb7e5a6d7
Christopher Henderson <chris.henderson1999@gmail.com>
2021-02-20 22:40:10 +0000

    Comments for navmeshagent

--- Legitimate Game/Assets/Scripts/NPCController.cs
+++ Legitimate Game/Assets/Scripts/NPCController.cs
@@ -6,18 +6,23 @@ using UnityEngine.AI;
 public class NPCController : MonoBehaviour
 {
 
+/*Where the NPC agent should be moving towards i.e. the player*/
 public Transform agentTarget;
+
+/*Make an agent that is capable of manouevring the navmesh*/
 NavMeshAgent navigateTo;
 
 
     void Start()
     {
+        /*Gets the navmeshagent component from the object this script is attached to*/
         navigateTo = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        /*Set the intended destination for the agent i.e the players position at this frame*/
         navigateTo.SetDestination(agentTarget.position);
     }
 }
