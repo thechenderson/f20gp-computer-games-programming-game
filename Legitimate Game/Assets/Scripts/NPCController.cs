b10fd31c33216cefeb2f0ba46a23c2a9d5ced52c
Christopher Henderson <chris.henderson1999@gmail.com>
2021-02-20 19:51:27 +0000

    Added a few colours to objects and added navmesh for agent to follow player

---
+++ Legitimate Game/Assets/Scripts/NPCController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCController : MonoBehaviour
+{
+
+public Transform agentTarget;
+NavMeshAgent navigateTo;
+
+
+    void Start()
+    {
+        navigateTo = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navigateTo.SetDestination(agentTarget.position);
+    }
+}
